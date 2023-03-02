4cc97837460edf16fc5be391fcb0e342b82c3ee7
Guru Mahan <iosdeve2022@gmail.com>
2023-03-02 16:13:36 +0530

    Initial Commit

---
+++ DigiClassOTP_POC/DigiClassOTP_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigiClassOTP_POCApp.swift
+//  DigiClassOTP_POC
+//
+//  Created by Guru Mahan on 02/03/23.
+//
+
+import SwiftUI
+
+@main
+struct DigiClassOTP_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
